ad42ecb94b441e7b2c1a8fdee6a73b53e1dd5af6
Andrew Snyder <andrew@andrews-mbp.lan>
2024-08-05 21:46:00 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line } from "react-icons/ri";
 import { RiCloseLargeLine } from "react-icons/ri";
 
@@ -10,31 +10,31 @@ const Navbar = () => {
     setNav(!nav)
   }
 
-  return (  
+  return (
     <div className='shadow w-full bg-white'>
-    <div className='text-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-       <h1 className='w-full text-3xl font-bold'>.andrew</h1>
+      <div className='text-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+        <h1 className='w-full text-3xl font-bold'>.andrew</h1>
         <ul className='hidden md:flex text-black space-x-12 font-sans'>
           <li className='p-4'><a>About</a></li>
           <li className='p-4'><a href='/work.js'>Work</a></li>
           <li className='p-4'><a>About</a></li>
         </ul>
 
-{/*Mobile Menu Button*/}
+        {/*Mobile Menu Button*/}
         <div onClick={handleNav} className='block md:hidden'>
-          {!nav ? <RiCloseLargeLine size={20}/> : < RiMenu3Line size={20}/> }
-          </div>
+          {!nav ? <RiCloseLargeLine size={20} /> : < RiMenu3Line size={20} />}
+        </div>
 
-{/*Mobile Menu Items*/}
+        {/*Mobile Menu Items*/}
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-50 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold m-4'>.andrew</h1>
+          <h1 className='w-full text-3xl font-bold m-4'>.andrew</h1>
           <ul className='uppercase p-4'>
             <li className='p-4 border-b'><a>About</a></li>
             <li className='p-4 border-b'><a href='/work.js'>Work</a></li>
             <li className='p-4'><a>About</a></li>
           </ul>
-          </div> 
-    </div>
+        </div>
+      </div>
     </div>
   );
 }
