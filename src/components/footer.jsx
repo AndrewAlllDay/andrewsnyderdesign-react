62aea0a7d7c456badfc86e56acef9de84b118347
“AndrewAlllDay” <andrewsnyderdesign@gmail.com>
2024-08-07 06:06:08 -0400

    Adds footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <div className='w-full h-[150px] border-t-2 mt-64 md:mt-4 border-gray-200'>
+      <div className='text-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 font-Poppins'>
+        <h1 className='w-full text-3xl font-bold'>.andrew</h1>
+        <ul className='hidden md:flex text-black space-x-12 font-Poppins'>
+          <li className='p-4'><a href='src/About.js'>About</a></li>
+          <li className='p-4'><a href='/work.js'>Work</a></li>
+          <li className='p-4'><a>Contact</a></li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
